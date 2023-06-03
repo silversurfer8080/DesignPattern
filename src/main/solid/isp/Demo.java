43410257b4393acd787067390bafd4d71481fdca
silversurfer8080 <gilberto01@hotmail.com>
2023-06-03 19:42:35 -0300

    Design Pattern - LSP, ISP, DIP

---
+++ src/main/solid/isp/Demo.java
@@ -0,0 +1,7 @@
+package isp;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/solid/lsp/Demo.java
@@ -0,0 +1,7 @@
+package lsp;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
